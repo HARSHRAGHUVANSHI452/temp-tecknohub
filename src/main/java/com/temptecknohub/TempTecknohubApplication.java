1c1049d1003823d279e3605972b92508a0a3c9be
HARSHRAGHUVANSHI452 <62739949+HARSHRAGHUVANSHI452@users.noreply.github.com>
2021-07-03 13:03:27 +0530

    harsh

---
+++ src/main/java/com/temptecknohub/TempTecknohubApplication.java
@@ -0,0 +1,13 @@
+package com.temptecknohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempTecknohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempTecknohubApplication.class, args);
+	}
+
+}
